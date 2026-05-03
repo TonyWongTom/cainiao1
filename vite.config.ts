a429e5bb9c5641081f572b9b44942c0bfd1c1403
TonyWongTom <bjhpyh1@gmail.com>
2026-05-03 22:19:46 +0800

    feat: Update Vite server configuration
    Change default host to true and port to 8080 for improved development server accessibility and configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,9 @@ export default defineConfig(({ mode }) => {
     return {
       base: '/',
       server: {
-        port: 3000,
-        host: '0.0.0.0',
+        host: true,
         allowedHosts: true,
+        port: 8080,
       },
       plugins: [
         react(),
